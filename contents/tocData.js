1dfa5e08ef992194559c95c23dfb2fa869543e98
Dystify <Dystify@gmail.com>
2017-02-13 20:30:42 +0100

    ok

---
+++ contents/tocData.js
@@ -0,0 +1,161 @@
+﻿// ConvertInfo : FairlyLinguist 1.5.0.0 - 2016/07/26 19:43:38
+{
+  var objTOC = new TocObject();
+  objTOC.setTitle("Overview<br /> - Private - ");
+  objTOC.setWindowTitle("Overview - Private - ");
+  objTOC.disablePrintPage();
+  objTOC.setSelectPackageEnabled(false);
+  objTOC.setInsertTitlePage(true);
+  objTOC.disableDisplayNumber();
+  objTOC.disableViewFileIcon();
+  objTOC.setBaselevel(0);
+  objTOC.setSearchEnabled(true);
+  objTOC.setLogoFile("template/img/logo.png");
+
+// 1 Introduction
+  objTOC.addHeader("ID_95696794", 0, "1 Introduction", "Pages/Page_95696794.html", "");
+
+// 2 Features
+  objTOC.addHeader("ID_162944925", 0, "2 Features", "Pages/Page_162944925.html", "");
+
+// 2.1 JOY-CON Play Styles
+  objTOC.addItem("ID_162944925", 1, "2.1 JOY-CON Play Styles", "Pages/Page_162946811.html", "");
+
+// 2.2 Application Features
+  objTOC.addItem("ID_162944925", 1, "2.2 Application Features", "Pages/Page_162946821.html", "");
+
+// 3 Hardware
+  objTOC.addHeader("ID_94755765", 0, "3 Hardware", "Pages/Page_94755765.html", "");
+
+// 3.1 System Hardware Configuration
+  objTOC.addItem("ID_94755765", 1, "3.1 System Hardware Configuration", "Pages/Page_95689155.html", "");
+
+// 3.2 JOY-CON Hardware Configuration
+  objTOC.addItem("ID_94755765", 1, "3.2 JOY-CON Hardware Configuration", "Pages/Page_95689161.html", "");
+
+// 3.3 Hardware Specifications
+  objTOC.addItem("ID_94755765", 1, "3.3 Hardware Specifications", "Pages/Page_124923644.html", "");
+
+// 3.4 Peripheral Hardware
+  objTOC.addItem("ID_94755765", 1, "3.4 Peripheral Hardware", "Pages/Page_95689337.html", "");
+
+// 4 Accounts
+  objTOC.addHeader("ID_95699326", 0, "4 Accounts", "Pages/Page_95699326.html", "");
+
+// 4.1 The NX and Accounts
+  objTOC.addItem("ID_95699326", 1, "4.1 The NX and Accounts", "Pages/Page_105383714.html", "");
+
+// 4.2 Users
+  objTOC.addItem("ID_95699326", 1, "4.2 Users", "Pages/Page_105383766.html", "");
+
+// 4.3 Nintendo Accounts
+  objTOC.addItem("ID_95699326", 1, "4.3 Nintendo Accounts", "Pages/Page_105383775.html", "");
+
+// 4.4 Friends
+  objTOC.addItem("ID_95699326", 1, "4.4 Friends", "Pages/Page_105383736.html", "");
+
+// 5 Console Features
+  objTOC.addHeader("ID_95708185", 0, "5 Console Features", "Pages/Page_95708185.html", "");
+
+// 5.1 System Menus
+  objTOC.addItem("ID_95708185", 1, "5.1 System Menus", "Pages/Page_91219109.html", "");
+
+// 5.1.1 Entrance
+  objTOC.addItem("ID_95708185", 2, "5.1.1 Entrance", "Pages/Page_91219176.html", "");
+
+// 5.1.2 HOME Menu
+  objTOC.addItem("ID_95708185", 2, "5.1.2 HOME Menu", "Pages/Page_91219165.html", "");
+
+// 5.1.3 Full Launcher
+  objTOC.addItem("ID_95708185", 2, "5.1.3 Full Launcher", "Pages/Page_91219172.html", "");
+
+// 5.2 System Application
+  objTOC.addItem("ID_95708185", 1, "5.2 System Application", "Pages/Page_91219634.html", "");
+
+// 5.2.1 My Page
+  objTOC.addItem("ID_95708185", 2, "5.2.1 My Page", "Pages/Page_91219211.html", "");
+
+// 5.2.2 News
+  objTOC.addItem("ID_95708185", 2, "5.2.2 News", "Pages/Page_91219208.html", "");
+
+// 5.2.3 Nintendo eShop
+  objTOC.addItem("ID_95708185", 2, "5.2.3 Nintendo eShop", "Pages/Page_104483077.html", "");
+
+// 5.2.4 Album
+  objTOC.addItem("ID_95708185", 2, "5.2.4 Album", "Pages/Page_91219494.html", "");
+
+// 5.2.5 System Settings
+  objTOC.addItem("ID_95708185", 2, "5.2.5 System Settings", "Pages/Page_91219526.html", "");
+
+// 5.3 Library Applets
+  objTOC.addItem("ID_95708185", 1, "5.3 Library Applets", "Pages/Page_91219642.html", "");
+
+// 5.3.1 Account Management Applet
+  objTOC.addItem("ID_95708185", 2, "5.3.1 Account Management Applet", "Pages/Page_107333950.html", "");
+
+// 5.3.2 Controller Support Applet
+  objTOC.addItem("ID_95708185", 2, "5.3.2 Controller Support Applet", "Pages/Page_107333963.html", "");
+
+// 5.3.3 Software Keyboard Applet
+  objTOC.addItem("ID_95708185", 2, "5.3.3 Software Keyboard Applet", "Pages/Page_107333976.html", "");
+
+// 5.3.4 My Page Applet
+  objTOC.addItem("ID_95708185", 2, "5.3.4 My Page Applet", "Pages/Page_162929043.html", "");
+
+// 5.3.5 Create or Edit Mii Applet
+  objTOC.addItem("ID_95708185", 2, "5.3.5 Create or Edit Mii Applet", "Pages/Page_107333984.html", "");
+
+// 5.3.6 amiibo Settings Applet
+  objTOC.addItem("ID_95708185", 2, "5.3.6 amiibo Settings Applet", "Pages/Page_107333993.html", "");
+
+// 5.3.7 EC Applet
+  objTOC.addItem("ID_95708185", 2, "5.3.7 EC Applet", "Pages/Page_107334016.html", "");
+
+// 5.3.8 Web Applet
+  objTOC.addItem("ID_95708185", 2, "5.3.8 Web Applet", "Pages/Page_107334022.html", "");
+
+// 5.3.9 Offline Web Applet
+  objTOC.addItem("ID_95708185", 2, "5.3.9 Offline Web Applet", "Pages/Page_124015154.html", "");
+
+// 5.3.10 Error Viewer Applet
+  objTOC.addItem("ID_95708185", 2, "5.3.10 Error Viewer Applet", "Pages/Page_107334003.html", "");
+
+// 5.4 Other Features
+  objTOC.addItem("ID_95708185", 1, "5.4 Other Features", "Pages/Page_96548908.html", "");
+
+// 5.4.1 Overlay Notifications
+  objTOC.addItem("ID_95708185", 2, "5.4.1 Overlay Notifications", "Pages/Page_91219187.html", "");
+
+// 5.4.2 Fatal Error
+  objTOC.addItem("ID_95708185", 2, "5.4.2 Fatal Error", "Pages/Page_91220654.html", "");
+
+// 5.5 Setting and Checking From Smart Device Apps
+  objTOC.addItem("ID_95708185", 1, "5.5 Setting and Checking From Smart Device Apps", "Pages/Page_91220656.html", "");
+
+// 6 Communication Features
+  objTOC.addHeader("ID_96539262", 0, "6 Communication Features", "Pages/Page_96539262.html", "");
+
+// 6.1 Data Distribution and Transmission System (BCAT)
+  objTOC.addItem("ID_96539262", 1, "6.1 Data Distribution and Transmission System (BCAT)", "Pages/Page_115068445.html", "");
+
+// 6.2 The Nintendo-provided communication features (NEX)
+  objTOC.addItem("ID_96539262", 1, "6.2 The Nintendo-provided communication features (NEX)", "Pages/Page_162924851.html", "");
+
+// 7 E-Commerce
+  objTOC.addHeader("ID_159226689", 0, "7 E-Commerce", "Pages/Page_159226689.html", "");
+
+// 7.1 E-Commerce Features Provided by NX
+  objTOC.addItem("ID_159226689", 1, "7.1 E-Commerce Features Provided by NX", "Pages/Page_164693456.html", "");
+
+// 8 Development Environment
+  objTOC.addHeader("ID_94755783", 0, "8 Development Environment", "Pages/Page_94755783.html", "");
+
+// 9 Appendix: Comparing With Previous Hardware
+  objTOC.addHeader("ID_95696926", 0, "9 Appendix: Comparing With Previous Hardware", "Pages/Page_95696926.html", "");
+
+
+  objTOC.addHeader("HISTORY", 0, "Revision History", "history.html", "", false);
+
+  objTOC.processTreeData();
+  Reassemble.objectTocData = objTOC;
+}
