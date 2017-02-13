1fe3bebfcf56ea86f089185923a651e3d9b2d89c
Dystify <Dystify@gmail.com>
2017-02-13 20:32:32 +0100

    ok

---
+++ NX Overview/contents/tocData.js
@@ -0,0 +1,112 @@
+﻿// ConvertInfo : FairlyLinguist 1.7.0.0 - 2016/10/11 11:59:00
+{
+  var objTOC = new TocObject();
+  objTOC.setTitle("System Features Guide<br /> - Private - ");
+  objTOC.setWindowTitle("System Features Guide - Private - ");
+  objTOC.disablePrintPage();
+  objTOC.setSelectPackageEnabled(false);
+  objTOC.setInsertTitlePage(true);
+  objTOC.disableDisplayNumber();
+  objTOC.disableViewFileIcon();
+  objTOC.setBaselevel(0);
+  objTOC.setSearchEnabled(true);
+  objTOC.setLogoFile("template/img/logo.png");
+
+// 1 Introduction
+  objTOC.addHeader("ID_91223668", 0, "1 Introduction", "Pages/Page_91223668.html", "", false, "001");
+
+// 2 System Menus
+  objTOC.addHeader("ID_91223874", 0, "2 System Menus", "Pages/Page_91223874.html", "", false, "002");
+
+// 2.1 Entrance
+  objTOC.addItem("ID_91223874", 1, "2.1 Entrance", "Pages/Page_126567299.html", "", false, "002_001");
+
+// 2.2 HOME Menu
+  objTOC.addItem("ID_91223874", 1, "2.2 HOME Menu", "Pages/Page_107331493.html", "", false, "002_002");
+
+// 2.2.1 Options
+  objTOC.addItem("ID_91223874", 2, "2.2.1 Options", "Pages/Page_107331498.html", "", false, "002_002_001");
+
+// 2.3 Full Launcher
+  objTOC.addItem("ID_91223874", 1, "2.3 Full Launcher", "Pages/Page_107331500.html", "", false, "002_003");
+
+// 2.4 Quick Settings
+  objTOC.addItem("ID_91223874", 1, "2.4 Quick Settings", "Pages/Page_115049565.html", "", false, "002_004");
+
+// 2.5 POWER Menu
+  objTOC.addItem("ID_91223874", 1, "2.5 POWER Menu", "Pages/Page_115049571.html", "", false, "002_005");
+
+// 3 System Application
+  objTOC.addHeader("ID_91223877", 0, "3 System Application", "Pages/Page_91223877.html", "", false, "003");
+
+// 3.1 Game News
+  objTOC.addItem("ID_91223877", 1, "3.1 Game News", "Pages/Page_126567292.html", "", false, "003_001");
+
+// 3.2 Nintendo eShop
+  objTOC.addItem("ID_91223877", 1, "3.2 Nintendo eShop", "Pages/Page_107323151.html", "", false, "003_002");
+
+// 3.3 Captures and Albums
+  objTOC.addItem("ID_91223877", 1, "3.3 Captures and Albums", "Pages/Page_106363296.html", "", false, "003_003");
+
+// 3.4 My Page
+  objTOC.addItem("ID_91223877", 1, "3.4 My Page", "Pages/Page_107323155.html", "", false, "003_004");
+
+// 3.5 System Settings
+  objTOC.addItem("ID_91223877", 1, "3.5 System Settings", "Pages/Page_95701204.html", "", false, "003_005");
+
+// 4 Library Applets
+  objTOC.addHeader("ID_91223882", 0, "4 Library Applets", "Pages/Page_91223882.html", "", false, "004");
+
+// 4.1 Account Management Applet
+  objTOC.addItem("ID_91223882", 1, "4.1 Account Management Applet", "Pages/Page_107323173.html", "", false, "004_001");
+
+// 4.2 Controller Support Applet
+  objTOC.addItem("ID_91223882", 1, "4.2 Controller Support Applet", "Pages/Page_107323180.html", "", false, "004_002");
+
+// 4.3 Software Keyboard Applet
+  objTOC.addItem("ID_91223882", 1, "4.3 Software Keyboard Applet", "Pages/Page_172887793.html", "", false, "004_003");
+
+// 4.4 My Page Applet
+  objTOC.addItem("ID_91223882", 1, "4.4 My Page Applet", "Pages/Page_166043916.html", "", false, "004_004");
+
+// 4.5 Mii Applet
+  objTOC.addItem("ID_91223882", 1, "4.5 Mii Applet", "Pages/Page_107323189.html", "", false, "004_005");
+
+// 4.6 amiibo Settings Applet
+  objTOC.addItem("ID_91223882", 1, "4.6 amiibo Settings Applet", "Pages/Page_107323194.html", "", false, "004_006");
+
+// 4.7 EC Applet
+  objTOC.addItem("ID_91223882", 1, "4.7 EC Applet", "Pages/Page_107323161.html", "", false, "004_007");
+
+// 4.8 Web Applet
+  objTOC.addItem("ID_91223882", 1, "4.8 Web Applet", "Pages/Page_107323164.html", "", false, "004_008");
+
+// 4.9 Offline Web Applet
+  objTOC.addItem("ID_91223882", 1, "4.9 Offline Web Applet", "Pages/Page_115049609.html", "", false, "004_009");
+
+// 4.10 Error Viewer Applet
+  objTOC.addItem("ID_91223882", 1, "4.10 Error Viewer Applet", "Pages/Page_107323197.html", "", false, "004_010");
+
+// 5 Other Features
+  objTOC.addHeader("ID_91223890", 0, "5 Other Features", "Pages/Page_91223890.html", "", false, "005");
+
+// 5.1 Overlay Notifications
+  objTOC.addItem("ID_91223890", 1, "5.1 Overlay Notifications", "Pages/Page_107323205.html", "", false, "005_001");
+
+// 5.2 Fatal Error
+  objTOC.addItem("ID_91223890", 1, "5.2 Fatal Error", "Pages/Page_107323211.html", "", false, "005_002");
+
+// 6 Shared Data
+  objTOC.addHeader("ID_107331287", 0, "6 Shared Data", "Pages/Page_107331287.html", "", false, "006");
+
+// 6.1 Mii
+  objTOC.addItem("ID_107331287", 1, "6.1 Mii", "Pages/Page_91223896.html", "", false, "006_001");
+
+// 6.2 System Fonts
+  objTOC.addItem("ID_107331287", 1, "6.2 System Fonts", "Pages/Page_91223899.html", "", false, "006_002");
+
+
+  objTOC.addHeader("HISTORY", 0, "Revision History", "history.html", "", false, "991");
+
+  Reassemble.objectTocData = objTOC;
+}
---
+++ NXSDEV/contents/tocData.js
@@ -0,0 +1,47 @@
+﻿// ConvertInfo : FairlyLinguist 1.6.0.0 - 2016/08/04 10:00:47
+{
+  var objTOC = new TocObject();
+  objTOC.setTitle("SDEV Quick Start<br /> - Private - ");
+  objTOC.setWindowTitle("SDEV Quick Start - Private - ");
+  objTOC.disablePrintPage();
+  objTOC.setSelectPackageEnabled(false);
+  objTOC.setInsertTitlePage(true);
+  objTOC.disableDisplayNumber();
+  objTOC.disableViewFileIcon();
+  objTOC.setBaselevel(0);
+  objTOC.setSearchEnabled(true);
+  objTOC.setLogoFile("template/img/logo.png");
+
+// 1 Introduction
+  objTOC.addHeader("ID_97625157", 0, "1 Introduction", "Pages/Page_97625157.html", "", false, "001");
+
+// 2 SDEV Operating Environment
+  objTOC.addHeader("ID_96563916", 0, "2 SDEV Operating Environment", "Pages/Page_96563916.html", "", false, "002");
+
+// 3 Preparing the SDEV
+  objTOC.addHeader("ID_123535845", 0, "3 Preparing the SDEV", "Pages/Page_123535845.html", "", false, "003");
+
+// 4 Preparing Nintendo Dev Interface
+  objTOC.addHeader("ID_96563737", 0, "4 Preparing Nintendo Dev Interface", "Pages/Page_96563737.html", "", false, "004");
+
+// 5 Preparing NintendoSDK
+  objTOC.addHeader("ID_96563581", 0, "5 Preparing NintendoSDK", "Pages/Page_96563581.html", "", false, "005");
+
+// 6 Initializing the SDEV
+  objTOC.addHeader("ID_96563591", 0, "6 Initializing the SDEV", "Pages/Page_96563591.html", "", false, "006");
+
+// 7 Preparing the JOY-CONs
+  objTOC.addHeader("ID_124920044", 0, "7 Preparing the JOY-CONs", "Pages/Page_124920044.html", "", false, "007");
+
+// 8 Building and Running NX Applications (For Developers)
+  objTOC.addHeader("ID_96565538", 0, "8 Building and Running NX Applications (For Developers)", "Pages/Page_96565538.html", "", false, "008");
+
+// 9 Running NX Applications (For Non-Developers)
+  objTOC.addHeader("ID_96563598", 0, "9 Running NX Applications (For Non-Developers)", "Pages/Page_96563598.html", "", false, "009");
+
+
+  objTOC.addHeader("HISTORY", 0, "Revision History", "history.html", "", false, "999");
+
+  objTOC.processTreeData();
+  Reassemble.objectTocData = objTOC;
+}
